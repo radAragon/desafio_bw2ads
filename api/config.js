746f0651e89488cf37065b3f1ac10c2d06891933
Radamés Aragón <radames.aragon@concrete.com.br>
2018-05-17 19:21:56 -0300

    Altera formato de configuração de Endpoints

--- api/config.js
+++ api/config.js
@@ -7,6 +7,7 @@ module.exports = {
     username: process.env.DB_USER || 'desafio',
     password: process.env.DB_PASS || 'mudar123'
   },
+
   queue: {
     protocol: 'amqp',
     hostname: process.env.RABBITMQ_HOST || 'localhost',
@@ -15,10 +16,19 @@ module.exports = {
     password: process.env.RABBITMQ_PASS || 'mudar123',
     name: process.env.RABBITMQ_QUEUE || 'fila1'
   },
-  ticketControlEndpoint:
-    `${process.env.TICKET_CONTROL_HOST || 'localhost'}:${process.env.TICKET_CONTROL_PORT || 3002}/api/v1/tickets`,
-  valueControleEndpoint:
-    `${process.env.VALUE_CONTROL_HOST || 'localhost'}:${process.env.VALUE_CONTROL_PORT || 3003}/api/v1/valores`
+
+  ticketControlEndpoint: {
+    hostname: process.env.TICKET_CONTROL_HOST || 'localhost',
+    port: parseInt(process.env.TICKET_CONTROL_PORT) || 3002,
+    path: '/api/v1/tickets'
+  },
+
+  valueControleEndpoint: {
+    hostname: process.env.VALUE_CONTROL_HOST || 'localhost',
+    port: parseInt(process.env.VALUE_CONTROL_PORT) || 3003,
+    path: '/api/v1/valores'
+  }
+
 }
 
 console.log('Setup:')
